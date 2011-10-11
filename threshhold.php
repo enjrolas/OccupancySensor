6ed40fa8e5000f4cf2383fd4da8d4226e2e4dc0c
alex <japhy@li64-162.members.linode.com>
2011-10-11 03:05:45 -0500

    sigh, git rm is a pain

--- threshhold.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("connect.php");
-$threshhold=$_REQUEST['threshhold'];
-echo $threshhold;
-$query="update threshhold set threshhold='$threshhold'";
-mysql_query($query) or die(mysql_error());
-?>
